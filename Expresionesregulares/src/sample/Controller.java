62ab4d30dece870a03816501c68631d9b6cb04d0
Laura-Esmeralda <71734279+Laura-Esmeralda@users.noreply.github.com>
2020-11-20 19:51:44 -0700

    practica1

--- Expresionesregulares/src/sample/Controller.java
+++ Expresionesregulares/src/sample/Controller.java
@@ -14,7 +14,9 @@ public class Controller {
     @FXML ListView listEje;
     @FXML Label lblResultado;
     @FXML protected void initialize(){
-        listEje.getItems().addAll("Ejercicio 1", "Ejercicio 2", "Validar Grupo", "Palabra con guión");
+        listEje.getItems().addAll("Ejercicio 1", "Ejercicio 2", "Validar Grupo", "Palabra con guión",
+                "Ejercicio 5", "palabra con count", "dos nombres", "MAC address", "Ejercicio 9", "número binario",
+                "número hexadecimal", "Operación", "número de control", "CURP");
     }
     public void procesar(ActionEvent event){
         int seleccion=listEje.getSelectionModel().getSelectedIndex();
@@ -35,6 +37,46 @@ public class Controller {
             }
             case 3:{
                 expresion="([a-zA-Z]{1,})[_]([a-zA-Z]{1,})";
+                break;
+            }
+            case 4:{
+                expresion="^[^db]([a-zA-Z]{3,})";
+                break;
+            }
+            case 5:{
+                expresion="([a-zA-Z]{3,})[count]$";
+                break;
+            }
+            case 6:{
+                expresion="([a-zA-Z]{1,10})[ ]([a-zA-Z]{1,10})";
+                break;
+            }
+            case 7:{
+                expresion="([0-9]{1,})[.]([0-9]{1,})[.]([0-9]{1,})[.]([0-9]{1,})";
+                break;
+            }
+            case 8:{
+                expresion="(^[A-Z]{1})([a-zA-Z]{1,})([a-zA-Z]{1}$)";
+                break;
+            }
+            case 9:{
+                expresion="([0-1]{8})";
+                break;
+            }
+            case 10:{
+                expresion="([0-9A-F]{1,})";
+                break;
+            }
+            case 11:{
+                expresion="(^[0-9]{1})[-|+|*|/|^]([0-9]{1})";
+                break;
+            }
+            case 12:{
+                expresion="(^[0-9]{2})([CG|cg]{2})([0-9]{4})";
+                break;
+            }
+            case 13:{
+                expresion="(^[A-Z]{4})([0-9]{6})([A-Z]{6})([0-9]{2})";
                 break;
             }
         }
